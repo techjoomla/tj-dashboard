a9ffd1c9550e521381d5c5b7b1a5a6905a80a8c9
“Renuka <renuka_s@tekditechnologies.com>
2017-10-23 15:03:07 +0530

    Adding the tjdashboard folder structure

---
+++ tjdashboard/code/administrator/sql/install.mysql.utf8.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS `#__tj_dashboard` (
+`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+
+`asset_id` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+
+`ordering` INT(11)  NOT NULL ,
+`state` TINYINT(1)  NOT NULL ,
+`checked_out` INT(11)  NOT NULL ,
+`checked_out_time` DATETIME NOT NULL ,
+`created_by` INT(11)  NOT NULL ,
+`modified_by` INT(11)  NOT NULL ,
+`title` VARCHAR(255)  NOT NULL ,
+PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT COLLATE=utf8_general_ci;
+
