3445fece4fdf072ef85476850a90042958c986e1
“Sudhir <sudhir_s@tekditechnologies.com>
2017-12-04 15:47:10 +0530

    Task #114298 fix: Create List view of Dashboards At Backend(Created By column added)

--- tjdashboard/administrator/views/dashboards/tmpl/default.php
+++ tjdashboard/administrator/views/dashboards/tmpl/default.php
@@ -42,7 +42,11 @@ if ($saveOrder)
 					// Search tools bar
 					echo JLayoutHelper::render('joomla.searchtools.default', array('view' => $this));
 					?>
-
+					<?php if (empty($this->items)) : ?>
+						<div class="alert alert-no-items">
+							<?php echo JText::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
+						</div>
+					<?php else : ?>
 					<table class="table table-striped" id="dashboardsList">
 						<thead>
 							<tr>
@@ -64,105 +68,87 @@ if ($saveOrder)
 									<?php echo JHtml::_('searchtools.sort', 'COM_TJDASHBOARD_LIST_VIEW_TITLE', 'dash.title', $listDirn, $listOrder); ?>
 								</th>
 								<th>
+									<?php echo JHtml::_('searchtools.sort', 'COM_TJDASHBOARD_LIST_VIEW_CREATEDBY', 'dash.created_by', $listDirn, $listOrder); ?>
+								</th>
+								<th>
 									<?php echo JHtml::_('searchtools.sort', 'COM_TJDASHBOARD_LIST_VIEW_ID', 'dash.dashboard_id', $listDirn, $listOrder); ?>
 								</th>
 							</tr>
 						</thead>
-
-						<tbody>
-							<?php
-							if(!empty($this->items))
-							{
-								foreach ($this->items as $i => $item)
-								{
-									$item->max_ordering = 0;
-									$ordering   = ($listOrder == 'dash.ordering');
-									$canCreate  = $this->canCreate; //user->authorise('core.create',     'com_content.category.' . $item->catid);
-									$canEdit    = $this->canEdit; // $user->authorise('core.edit',       'com_content.article.' . $item->id);
-
-									$canCheckin = $this->canCheckin; //$user->authorise('core.manage',     'com_checkin') || $item->checked_out == $userId || $item->checked_out == 0;
-									// $canEditOwn = $user->authorise('core.edit.own',   'com_content.article.' . $item->id) && $item->created_by == $userId;
-
-									$canChange  = $this->canChangeStatus; // $user->authorise('core.edit.state', 'com_content.article.' . $item->id) && $canCheckin;
-									?>
-
-									<tr class="row<?php echo $i % 2; ?>" sortable-group-id="<?php echo $item->dashboard_id; ?>">
-										<td class="order nowrap center hidden-phone">
-											<?php
-											$iconClass = '';
-
-											if (!$canChange)
-											{
-												$iconClass = ' inactive';
-											}
-											elseif (!$saveOrder)
-											{
-												$iconClass = ' inactive tip-top hasTooltip" title="' . JHtml::_('tooltipText', 'JORDERINGDISABLED');
-											}
-											?>
-											<span class="sortable-handler<?php echo $iconClass ?>">
-												<span class="icon-menu" aria-hidden="true"></span>
-											</span>
-											<?php if ($canChange && $saveOrder) : ?>
-												<input type="text" style="display:none" name="order[]" size="5" value="<?php echo $item->ordering; ?>" class="width-20 text-area-order" />
-											<?php endif; ?>
-										</td>
-
-										<td class="center">
-											<?php echo JHtml::_('grid.id', $i, $item->dashboard_id); ?>
-										</td>
-
-										<?php if (isset($this->items[0]->state)): ?>
-											<td class="center">
-												<?php echo JHtml::_('jgrid.published', $item->state, $i, 'dashboards.', $canChange, 'cb'); ?>
-											</td>
-										<?php endif; ?>
-
-										<td class="has-context">
-											<div class="pull-left break-word">
-												<?php if ($item->checked_out) : ?>
-													<?php echo JHtml::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time, 'dashboards.', $canCheckin); ?>
-												<?php endif; ?>
-
-												<?php if ($canEdit || $canEditOwn) : ?>
-													<a class="hasTooltip" href="<?php echo JRoute::_('index.php?option=com_tjdashboard&task=dashboard.edit&id=' . $item->dashboard_id); ?>" title="<?php echo JText::_('JACTION_EDIT'); ?>">
-														<?php echo $this->escape($item->title); ?></a>
-												<?php else : ?>
-													<span><?php echo $this->escape($item->title); ?></span>
-												<?php endif; ?>
-											</div>
-										</td>
-
-										<td><?php echo $item->dashboard_id; ?></td>
-									</tr>
-
-									<?php
-								}
-							}
-							else
-							{ ?>
-							<tfoot>
+						<tfoot>
 							<tr>
-								<!--td-->
-								<div class="alert alert-no-items">
-									<?php echo JText::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
-								</div>
-								<!--/td-->
-							</tr>
-							<tr>
-								
 								<td colspan="10">
-									<?php //echo $this->pagination->getListFooter(); ?>
+									<?php echo $this->pagination->getListFooter(); ?>
 								</td>
 							</tr>
-							</tfoot>
+						</tfoot>
+						<tbody>
 							<?php
-							}
+							foreach ($this->items as $i => $item)
+							{
+								$item->max_ordering = 0;
+								$ordering   = ($listOrder == 'dash.ordering');
+								$canCreate  = $this->canCreate; //user->authorise('core.create',     'com_content.category.' . $item->catid);
+								$canEdit    = $this->canEdit; // $user->authorise('core.edit',       'com_content.article.' . $item->id);
+
+								$canCheckin = $this->canCheckin; //$user->authorise('core.manage',     'com_checkin') || $item->checked_out == $userId || $item->checked_out == 0;
+									// $canEditOwn = $user->authorise('core.edit.own',   'com_content.article.' . $item->id) && $item->created_by == $userId;
+
+								$canChange  = $this->canChangeStatus; // $user->authorise('core.edit.state', 'com_content.article.' . $item->id) && $canCheckin;
+								?>
+								<tr class="row<?php echo $i % 2; ?>" sortable-group-id="<?php echo $item->dashboard_id; ?>">
+								<td class="order nowrap center hidden-phone">
+									<?php
+									$iconClass = '';
+									if (!$canChange)
+									{
+										$iconClass = ' inactive';
+									}
+									elseif (!$saveOrder)
+									{
+										$iconClass = ' inactive tip-top hasTooltip" title="' . JHtml::_('tooltipText', 'JORDERINGDISABLED');
+									}
+									?>
+									<span class="sortable-handler<?php echo $iconClass ?>">
+										<span class="icon-menu" aria-hidden="true"></span>
+									</span>
+									<?php if ($canChange && $saveOrder) : ?>
+									<input type="text" style="display:none" name="order[]" size="5" value="<?php echo $item->ordering; ?>" class="width-20 text-area-order" />
+									<?php endif; ?>
+								</td>
+								<td class="center">
+									<?php echo JHtml::_('grid.id', $i, $item->dashboard_id); ?>
+								</td>
+								<?php if (isset($this->items[0]->state)): ?>
+								<td class="center">
+									<?php echo JHtml::_('jgrid.published', $item->state, $i, 'dashboards.', $canChange, 'cb'); ?>
+								</td>
+								<?php endif; ?>
+								<td class="has-context">
+									<div class="pull-left break-word">
+										<?php if ($item->checked_out) : ?>
+										<?php echo JHtml::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time, 'dashboards.', $canCheckin); ?>
+										<?php endif; ?>
+										<?php if ($canEdit || $canEditOwn) : ?>
+											<a class="hasTooltip" href="<?php echo JRoute::_('index.php?option=com_tjdashboard&task=dashboard.edit&id=' . $item->dashboard_id); ?>" title="<?php echo JText::_('JACTION_EDIT'); ?>">
+											<?php echo $this->escape($item->title); ?></a>
+											<?php else : ?>
+											<span title="<?php echo JText::sprintf('JFIELD_ALIAS_LABEL', $this->escape($item->alias)); ?>"><?php echo $this->escape($item->title); ?></span>
+										<?php endif; ?>
+										<span class="small break-word">
+											<?php echo JText::sprintf('JGLOBAL_LIST_ALIAS', $this->escape($item->alias)); ?>
+										</span>
+									</div>
+								</td>
+								<td><?php echo $item->created_by; ?></td>
+								<td><?php echo $item->dashboard_id; ?></td>
+							</tr>
+							<?php
+								}
 							?>
 						<tbody>
 					</table>
-
-					<?php //echo $this->pagination->getListFooter(); ?>
+					<?php endif; ?>
 
 					<input type="hidden" name="task" value="" />
 					<input type="hidden" name="boxchecked" value="0" />
