eabeb6f4154937707de58f54b6dda229f9fa020d
Sudhir <sudhir_s@tekditechnologies.com>
2018-01-19 17:50:26 +0530

    Task #114311 fix: Add uniqe for alias

--- tjdashboard/administrator/sql/install.mysql.utf8.sql
+++ tjdashboard/administrator/sql/install.mysql.utf8.sql
@@ -27,6 +27,7 @@ ALTER TABLE `#__tj_dashboards`
   ADD KEY `idx_checkout` (`checked_out`),
   ADD KEY `idx_state` (`state`),
   ADD KEY `idx_createdby` (`created_by`),
+  ADD UNIQUE KEY `unqk_alias` (`alias`),
   ADD KEY `idx_alias` (`alias`);
 
 CREATE TABLE IF NOT EXISTS `#__tj_dashboard_widgets` (
@@ -46,7 +47,8 @@ CREATE TABLE IF NOT EXISTS `#__tj_dashboard_widgets` (
   `params` text NOT NULL,
   `size` int(11) NOT NULL COMMENT 'Screen size for widget (col-span-3/6/9/12)',
   `autorefresh` int(11) NOT NULL COMMENT 'Widget refresh time span in second',
-  PRIMARY KEY (`dashboard_widget_id`)
+  PRIMARY KEY (`dashboard_widget_id`),
+  FOREIGN KEY (`dashboard_id`) REFERENCES `#__tj_dashboards` (`dashboard_id`)
 );
 
 --
@@ -57,4 +59,4 @@ ALTER TABLE `#__tj_dashboard_widgets`
   ADD KEY `idx_dashboard_id` (`dashboard_id`),
   ADD KEY `idx_checkout` (`checked_out`),
   ADD KEY `idx_state` (`state`),
-  ADD KEY `idx_createdby` (`created_by`),
+  ADD KEY `idx_createdby` (`created_by`)
