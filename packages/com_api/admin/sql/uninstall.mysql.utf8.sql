c43820a3ffae4af6c01979a93fb617ed17c2c273
monika.singh <monika.singh@vowellms.com>
2018-04-18 18:50:46 +0530

    delet com api

--- packages/com_api/admin/sql/uninstall.mysql.utf8.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE IF EXISTS `#__api_keys`;
-DROP TABLE IF EXISTS `#__api_logs`;
--- packages/com_api/admin/sql/updates/mysql/2.1.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE #__api_keys ADD INDEX `userid` (userid);
-ALTER TABLE #__api_keys ADD INDEX `hash` (hash);
-ALTER TABLE #__api_logs ADD INDEX `hash` (hash);
