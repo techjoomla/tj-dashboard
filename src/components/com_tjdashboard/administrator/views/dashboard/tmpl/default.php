468f2171e7da66f50888b34baec0e74e3239fbb6
monika.singh <monika.singh@vowellms.com>
2018-05-22 14:27:32 +0530

    Task #127705 feat: Create new admin view to include default sidebar for any component(including shika)

--- src/components/com_tjdashboard/administrator/views/dashboard/tmpl/default.php
+++ src/components/com_tjdashboard/administrator/views/dashboard/tmpl/default.php
@@ -13,43 +13,44 @@ defined('_JEXEC') or die;
 $document = JFactory::getDocument();
 $script  = 'var root_url = "' . Juri::root() . '";';
 $document->addScriptDeclaration($script, 'text/javascript');
-$document->addScript(Juri::root().'plugins/tjdashboardrenderer/tabulator/assets/js/jquery-ui.min.js');
-$document->addStyleSheet(JUri::root().'media/com_tjdashboard/css/bootstrap3/css/bootstrap.min.css');
-$document->addStylesheet(Juri::root().'plugins/tjdashboardrenderer/tabulator/assets/css/tabulator_semantic-ui.min.css');
-$document->addStylesheet(Juri::root().'components/com_tjdashboard/assets/css/dashboard.css');
-$document->addStylesheet(Juri::root().'plugins/tjdashboardrenderer/countbox/assets/css/countbox.css');
+$document->addScript(Juri::root() . 'plugins/tjdashboardrenderer/tabulator/assets/js/jquery-ui.min.js');
+$document->addStyleSheet(JUri::root() . '/media/techjoomla_strapper/bs3/css/bootstrap.css');
+$document->addStyleSheet(JUri::root() . 'media/com_tjdashboard/css/font-awesome.min.css');
+$document->addStylesheet(Juri::root() . 'plugins/tjdashboardrenderer/tabulator/assets/css/tabulator_semantic-ui.min.css');
+$document->addStylesheet(Juri::root() . 'components/com_tjdashboard/assets/css/dashboard.css');
+$document->addStylesheet(Juri::root() . 'plugins/tjdashboardrenderer/countbox/assets/css/countbox.css');
 
-$document->addScript(Juri::root().'components/com_tjdashboard/assets/js/tjDashboardService.js');
-$document->addScript(Juri::root().'components/com_tjdashboard/assets/js/tjDashboardUI.js');
-$document->addStylesheet(Juri::root().'media/com_tjdashboard/css/tjdashboard-sb-admin.css');
-
+$document->addScript(Juri::root() . 'components/com_tjdashboard/assets/js/tjDashboardService.min.js');
+$document->addScript(Juri::root() . 'components/com_tjdashboard/assets/js/tjDashboardUI.min.js');
+$document->addStylesheet(Juri::root() . 'media/com_tjdashboard/css/tjdashboard-sb-admin.css');
 ?>
-<!--
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
+
 <script>
 jQuery(document).ready(function() {
-	TJDashboardUI.initDashboard(<?php echo $this->item->dashboard_id; ?>);
+	TJDashboardUI.initDashboard(<?php echo $this->item->dashboard_id;?>);
 });
 </script>
-<div class="tjBs3">
-			<!-- TJ Dashboard -->
-					<div class="tjDB">
-<div class="tj-page">
+
+<div class="tjBs3 tjlms-wrapper">
 	<div class="row-fluid">
-			<?php if (!empty( $this->sidebar)) : ?>
-				<div id="j-sidebar-container" class="span2">
-					<?php echo $this->sidebar; ?>
+		<?php if (!empty( $this->sidebar))
+		{
+		?>
+			 <div id="j-sidebar-container" class="span2">
+				<?php echo $this->sidebar; ?>
+			</div>
+			<div  id="j-main-container" class="span10">
+		<?php
+		}
+		else
+		{
+		?>
+			<div id="j-main-container" class="span12">
+		<?php
+		}
+		?>
+				<div class="tjdashboard" >
 				</div>
-				<div id="j-main-container" class="span10">
-			<?php else : ?>
-				<div id="j-main-container">
-			<?php endif; ?>
-			<div class="col-xs-12 tjdashboard" style="margin-left:10px;">
-			</div>
-
 			</div>
 	</div>
 </div>
-	</div>
-</div>
-
