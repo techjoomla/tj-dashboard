781c3fe1c2d74970b931d7da89f6ee7a3f12b990
monika.singh <monika.singh@vowellms.com>
2018-06-11 12:00:24 +0530

    Bug #128957 fix: Add single dashboard menu item >> incorrect link shown in menu

---
+++ src/components/com_tjdashboard/administrator/models/fields/dashboard.php
@@ -0,0 +1,179 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_contact
+ *
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('JPATH_BASE') or die;
+
+/**
+ * Supports a modal contact picker.
+ *
+ * @since  1.6
+ */
+class JFormFieldModal_Dashboard extends JFormField
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var     string
+	 * @since   1.6
+	 */
+	protected $type = 'Modal_Dashboard';
+
+	/**
+	 * Method to get the field input markup.
+	 *
+	 * @return  string  The field input markup.
+	 *
+	 * @since   1.6
+	 */
+	protected function getInput()
+	{
+		$allowClear  = ((string) $this->element['clear'] != 'false');
+		$allowSelect = ((string) $this->element['select'] != 'false');
+
+		// Load language
+		JFactory::getLanguage()->load('com_tjdashboard', JPATH_ADMINISTRATOR);
+
+		// The active dashboard id field.
+		$value = (int) $this->value > 0 ? (int) $this->value : '';
+
+		// Create the modal id.
+		$modalId = 'Dashboard_' . $this->id;
+
+		// Add the modal field script to the document head.
+		JHtml::_('jquery.framework');
+		JHtml::_('script', 'system/modal-fields.js', array('version' => 'auto', 'relative' => true));
+
+		// Script to proxy the select modal function to the modal-fields.js file.
+		if ($allowSelect)
+		{
+			static $scriptSelect = null;
+
+			if (is_null($scriptSelect))
+			{
+				$scriptSelect = array();
+			}
+
+			if (!isset($scriptSelect[$this->id]))
+			{
+				JFactory::getDocument()->addScriptDeclaration("
+				function jSelectDashboard_" . $this->id . "(id, title, object) {
+					window.processModalSelect('Dashboard', '" . $this->id . "', id, title, '', object);
+				}
+				");
+
+				$scriptSelect[$this->id] = true;
+			}
+		}
+
+		// Setup variables for display.
+		$linkDashboards = 'index.php?option=com_tjdashboard&amp;view=dashboards&amp;layout=modal&amp;tmpl=component&amp;' . JSession::getFormToken() . '=1';
+		$linkDashboard  = 'index.php?option=com_tjdashboard&amp;view=dashboard&amp;' . JSession::getFormToken() . '=1';
+		$modalTitle   = JText::_('COM_TJDASHBOARD_CHANGE_DASHBOARD');
+
+		if (isset($this->element['language']))
+		{
+			$linkDashboard   .= '&amp;forcedLanguage=' . $this->element['language'];
+			$modalTitle     .= ' &#8212; ' . $this->element['label'];
+		}
+
+		$urlSelect = $linkDashboards . '&amp;function=jSelectDashboard_' . $this->id;
+
+		$urlNew    = $linkdashboard . '&amp;task=dashboard.add';
+
+		if ($value)
+		{
+			$db    = JFactory::getDbo();
+			$query = $db->getQuery(true)
+				->select($db->quoteName('name'))
+				->from($db->quoteName('#__dashboard_details'))
+				->where($db->quoteName('id') . ' = ' . (int) $value);
+			$db->setQuery($query);
+
+			try
+			{
+				$title = $db->loadResult();
+			}
+			catch (RuntimeException $e)
+			{
+				JError::raiseWarning(500, $e->getMessage());
+			}
+		}
+
+		$title = empty($title) ? JText::_('COM_TJDASHBOARD_WIDGET_FORM_LBL_DASHBOARDID') : htmlspecialchars($title, ENT_QUOTES, 'UTF-8');
+
+		// The current dashboard display field.
+		$html  = '<span class="input-append">';
+		$html .= '<input class="input-medium" id="' . $this->id . '_name" type="text" value="' . $title . '" disabled="disabled" size="35" />';
+
+		// Select contact button
+		if ($allowSelect)
+		{
+			$html .= '<a'
+				. ' class="btn hasTooltip' . ($value ? ' hidden' : '') . '"'
+				. ' id="' . $this->id . '_select"'
+				. ' data-toggle="modal"'
+				. ' role="button"'
+				. ' href="#ModalSelect' . $modalId . '"'
+				. ' title="' . JHtml::tooltipText('COM_DASHBOARD_CHANGE_DASHBOARD') . '">'
+				. '<span class="icon-file" aria-hidden="true"></span> ' . JText::_('JSELECT')
+				. '</a>';
+		}
+
+		// Clear dashboard button
+		if ($allowClear)
+		{
+			$html .= '<a'
+				. ' class="btn' . ($value ? '' : ' hidden') . '"'
+				. ' id="' . $this->id . '_clear"'
+				. ' href="#"'
+				. ' onclick="window.processModalParent(\'' . $this->id . '\'); return false;">'
+				. '<span class="icon-remove" aria-hidden="true"></span>' . JText::_('JCLEAR')
+				. '</a>';
+		}
+
+		$html .= '</span>';
+
+		// Select dashboard modal
+		if ($allowSelect)
+		{
+			$html .= JHtml::_(
+				'bootstrap.renderModal',
+				'ModalSelect' . $modalId,
+				array(
+					'title'       => $modalTitle,
+					'url'         => $urlSelect,
+					'height'      => '400px',
+					'width'       => '800px',
+					'bodyHeight'  => '70',
+					'modalWidth'  => '80',
+					'footer'      => '<a role="button" class="btn" data-dismiss="modal" aria-hidden="true">' . JText::_('JLIB_HTML_BEHAVIOR_CLOSE') . '</a>',
+				)
+			);
+		}
+
+		$class = $this->required ? ' class="required modal-value"' : '';
+
+		$html .= '<input type="hidden" id="' . $this->id . '_id"' . $class . ' data-required="' . (int) $this->required . '" name="' . $this->name
+			. '" data-text="' . htmlspecialchars(JText::_('COM_DASHBOARD_SELECT_A_DASHBOARD', true), ENT_COMPAT, 'UTF-8') . '" value="' . $value . '" />';
+
+		return $html;
+	}
+
+	/**
+	 * Method to get the field label markup.
+	 *
+	 * @return  string  The field label markup.
+	 *
+	 * @since   3.4
+	 */
+	protected function getLabel()
+	{
+		return str_replace($this->id, $this->id . '_id', parent::getLabel());
+	}
+}
