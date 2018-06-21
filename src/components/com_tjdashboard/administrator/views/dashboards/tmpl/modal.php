fd556e4fb7a72184c4bb899433b17a379dcf7833
monika.singh <monika.singh@vowellms.com>
2018-06-21 14:28:19 +0530

    Bug #129468 fix: Giving warning on installing shika pacakage

--- src/components/com_tjdashboard/administrator/views/dashboards/tmpl/modal.php
+++ src/components/com_tjdashboard/administrator/views/dashboards/tmpl/modal.php
@@ -1,10 +1,9 @@
 <?php
 /**
- * @package     Joomla.Administrator
- * @subpackage  com_contact
- *
- * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @package    Com_Tjdashboard
+ * @author     Techjoomla <extensions@techjoomla.com>
+ * @copyright  2017 Techjoomla
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
