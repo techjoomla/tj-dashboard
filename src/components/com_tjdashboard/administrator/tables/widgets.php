c62d511722b9db3a4590e08e9506caaeefb137f7
shital <shital.kakade@vowellms.com>
2018-07-27 15:37:37 +0530

    Bug fix:  If Default widget is once deleted, then it is not visible even if new package is installed

--- src/components/com_tjdashboard/administrator/tables/widgets.php
+++ src/components/com_tjdashboard/administrator/tables/widgets.php
@@ -55,4 +55,45 @@ class TjdashboardTableWidgets extends JTable
 
 		return parent::check();
 	}
+
+	/**
+	 * Method to delete a Widget.
+	 *
+	 * @param   mixed  $pk  Primary key value to delete. Optional
+	 * 
+	 * @return  boolean  True on success.
+	 *
+	 * @since   1.0.0
+	 */
+	public function delete($pk = null)
+	{
+		JSession::checkToken() or jexit(JText::_('JINVALID_TOKEN'));
+		$result = 0;
+
+		if (!empty($pk))
+		{
+			try
+			{
+				$db = JFactory::getDbo();
+
+				// Check if widget is not default
+				$table = JTable::getInstance('widgets', 'TjdashboardTable', array('dbo', $db));
+
+				if ($table->load(array('dashboard_widget_id' => $pk)) && ($table->default_widget != 1))
+				{
+					$result = parent::delete($pk);
+				}
+				elseif ($table->load(array('dashboard_widget_id' => $pk)) && ($table->default_widget == 1))
+				{
+					JFactory::getApplication()->enqueueMessage(JText::_('COM_TJDASHBOARD_DEFAULT_WIDGETS_DELETE_ERROR_MESSAGE'), 'error');
+				}
+
+				return $result;
+			}
+			catch (Exception $e)
+			{
+				JFactory::getApplication()->enqueueMessage(JText::_('COM_TJDASHBOARD_WIDGETS_DELETE_ERROR_MESSAGE'), 'error');
+			}
+		}
+	}
 }
