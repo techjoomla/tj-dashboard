bbb02975729d79794a9ab483c55a845b0463181e
shital <shital.kakade@vowellms.com>
2018-09-21 14:43:51 +0530

    remove old widgets data from table

--- src/components/com_tjdashboard/administrator/sql/updates/mysql/1.1.0.sql
+++ src/components/com_tjdashboard/administrator/sql/updates/mysql/1.1.0.sql
@@ -1,1 +1,6 @@
+DELETE FROM `#__tj_dashboard_widgets`;
+ALTER TABLE `#__tj_dashboard_widgets` AUTO_INCREMENT = 1;
+DELETE FROM `#__tj_dashboards`;
+ALTER TABLE `#__tj_dashboards` AUTO_INCREMENT = 1;
+
 ALTER TABLE `#__tj_dashboard_widgets` Add `core` tinyint(1) NOT NULL DEFAULT '0' AFTER `autorefresh`;
