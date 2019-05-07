d5270738be506e3636724b6cac2c808b6b521910
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2019-05-07 15:30:25 +0530

    Issue #000 task: Skeleton loader cleanup

--- src/components/com_tjdashboard/administrator/views/dashboard/tmpl/default.php
+++ src/components/com_tjdashboard/administrator/views/dashboard/tmpl/default.php
@@ -44,11 +44,6 @@ jQuery(document).ready(function() {
 		<?php
 		}
 		?>
-				<div class="tjdashboardloadingwidget">
-					<div class="bounce1"></div>
-					<div class="bounce2"></div>
-					<div class="bounce3"></div>
-				</div>
 				<div class="tjdashboard" >
 				</div>
 			</div>
