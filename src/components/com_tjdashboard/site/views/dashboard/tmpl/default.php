4a2114d9c3a6b15f129c0880ada73b10967662c6
poonam_b <poonam_b@techjoomla.com>
2021-03-23 20:35:54 +0530

    Task #169307 chore: Create a custom renderer for filters

--- src/components/com_tjdashboard/site/views/dashboard/tmpl/default.php
+++ src/components/com_tjdashboard/site/views/dashboard/tmpl/default.php
@@ -11,15 +11,18 @@
 // No direct access
 defined('_JEXEC') or die;
 
-JHTML::_('behavior.modal');
-
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+HTMLHelper::_('behavior.modal');
+HTMLHelper::_('formbehavior.chosen', 'select');
+HTMLHelper::_('jquery.token');
 
 $document = JFactory::getDocument();
 
 $document->addStylesheet('components/com_tjdashboard/assets/css/dashboard.css');
 $document->addStylesheet('media/techjoomla_strapper/css/bootstrap.j3.min.css');
-$document->addScript('components/com_tjdashboard/assets/js/tjDashboardService.min.js');
-$document->addScript('components/com_tjdashboard/assets/js/tjDashboardUI.min.js');
+$document->addScript('components/com_tjdashboard/assets/js/tjDashboardService.js');
+$document->addScript('components/com_tjdashboard/assets/js/tjDashboardUI.js');
 $document->addStylesheet('media/com_tjdashboard/css/tjdashboard-sb-admin.css');
 ?>
 
@@ -33,9 +36,8 @@ jQuery(document).ready(function() {
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-xs-12 tjdashboard">
-				<h3>
-					<div data-dashboard-id="<?php echo $this->item->dashboard_id;?>" class="tjdashboard-title"><?php echo htmlspecialchars($this->item->title);?></div>
-				</h3>
+
+					<h3 class="d-none" data-dashboard-id="<?php echo $this->item->dashboard_id;?>"><?php echo htmlspecialchars($this->item->title);?></h3>
 			</div>
 		</div>
 	</div>
